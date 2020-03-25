52f520c464a2491cd643d884e0512e7d88105a78
michael innamorato <michaelinnamorato94@gmail.com>
2020-03-25 13:29:14 -0400

    Add files via upload

--- interactive_dict.py
+++ interactive_dict.py
@@ -8,6 +8,10 @@ def read_key(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:     # in case the first letter in capital "Capital"
+        return data[w.title()]
+    elif w.upper() in data:    # in case whole word is upper "USA""NATO"
+        return data[w.upper()]    
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(w,data.keys())[0])
         if yn == "Y" or yn == "y":
